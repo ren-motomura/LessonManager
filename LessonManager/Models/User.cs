10d1e97156645579a8dd003b1db5c584ab832395
ren.motomura <ren.motomura@gmail.com>
2017-08-28 22:31:20 +0900

    First Commit

---
+++ LessonManager/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonManager.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ LessonManager/WebAPIs/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace LessonManager.WebAPIs
+{
+    class Client
+    {
+        private static Client instance = new Client();
+
+        public static Client Instance
+        {
+            get { return instance; }
+        }
+
+        private HttpClient client;
+
+        private Client()
+        {
+            client = new HttpClient();
+        }
+    }
+}
